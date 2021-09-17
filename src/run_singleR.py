8ed1c22aa42dad138444a963b0176b9bfa3c90c5
namezhenzhang <704543161@qq.com>
2021-09-18 02:17:53 +0800

    修改代码

--- src/run_singleR.py
+++ src/run_singleR.py
@@ -1,3 +1,4 @@
+from torchvision.models import resnet34, resnet18, resnet101
 from tqdm import tqdm
 import h5py
 import numpy as np
@@ -6,64 +7,65 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torch.nn import functional as F
 import matplotlib.pyplot as plt
-# from model import *
+
 
 class sevidataset(Dataset):
-    def __init__(self, inputs,labels):
+    def __init__(self, inputs, labels):
         print("preparing dataset")
-        self.inputs = torch.Tensor(inputs)#.permute(0,2,1)
+        self.inputs = torch.Tensor(inputs) 
         self.labels = torch.Tensor(labels)
-        assert len(self.inputs)==len(self.labels),"lens of inputs & labels are not same."
-        self.tensor = {'inputs':self.inputs,'labels':self.labels}
+        assert len(self.inputs) == len(
+            self.labels), "lens of inputs & labels are not same."
+        self.tensor = {'inputs': self.inputs, 'labels': self.labels}
         print("finished")
+
     def __len__(self):
-        return len(self.inputs)    
+        return len(self.inputs)
 
     def __getitem__(self, index):
         return {key: data[index] for key, data in self.tensor.items()}
+
     def cuda(self):
         for key in self.tensor:
             self.tensor[key] = self.tensor[key].cuda()
+
     def cpu(self):
         for key in self.tensor:
             self.tensor[key] = self.tensor[key].cpu()
 
-def eval(model,dataloader):
+
+def eval(model, dataloader):
     loss_list = []
     with torch.no_grad():
         model.eval()
-        criterion = nn.MSELoss() 
-        
+        criterion = nn.MSELoss()
+
         with tqdm(total=len(dataloader)) as t:
             for step, batch in enumerate(dataloader):
-                
+
                 t.set_description("{}".format('@test'))
 
                 predict = model(batch['inputs'].unsqueeze(1))
                 labels = batch['labels']
-                # print(predict,labels)
-                # print(F.softmax(predict,dim=1),labels)
+
                 loss = criterion(predict, labels)
                 t.set_postfix(loss=loss.item())
                 t.update(1)
 
-                # if args.gradient_accumulation_steps > 1:
-                #     loss = loss / args.gradient_accumulation_steps
-
-
                 loss_list.append(loss.item())
     print(sum(loss_list)/len(loss_list))
 
-def loss_func(input,label):
 
-    rate = 2/(np.array(range(1,13))*4+1)
+def loss_func(input, label):
+
+    rate = 2/(np.array(range(1, 13))*4+1)
     rate = torch.Tensor(rate).cuda()
     delta = input - label
-    return torch.sum(torch.sum((delta**2 * rate),dim=1))/len(input)
+    return torch.sum(torch.sum((delta**2 * rate), dim=1))/len(input)
 
 
-def train(model,dataloader,testdataloader,max_epochs=1):
-    
+def train(model, dataloader, testdataloader, max_epochs=1):
+
     epochs = 0
     criterion = loss_func
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
@@ -76,80 +78,65 @@ def train(model,dataloader,testdataloader,max_epochs=1):
 
         with tqdm(total=len(dataloader)) as t:
             for step, batch in enumerate(dataloader):
-                
-                t.set_description("{},Epoch {}".format(model_idx,epochs))
+
+                t.set_description("{},Epoch {}".format(model_idx, epochs))
 
                 predict = model(batch['inputs'].unsqueeze(1))
                 labels = batch['labels']
-                # print(predict,labels)
-                # print(F.softmax(predict,dim=1),labels)
+
                 loss = criterion(predict, labels)
                 t.set_postfix(loss=loss.item())
                 t.update(1)
 
-                # if args.gradient_accumulation_steps > 1:
-                #     loss = loss / args.gradient_accumulation_steps
+
                 optimizer.zero_grad()
                 loss.backward()
                 loss_list.append(loss.item())
                 optimizer.step()
-                # tr_loss += loss.item()
 
-                # if (step + 1) % args.gradient_accumulation_steps == 0:
-                #     torch.nn.utils.clip_grad_norm_(self.model.parameters(), args.max_grad_norm)
-                #     self.optimizer.step()
-                #     self.scheduler.step()
-                #     self.optimizer_new_token.step()
-                #     self.scheduler_new_token.step()
-                #     self.model.zero_grad()
-                #     global_step += 1
         plt.plot(loss_list)
         print(sum(loss_list)/len(loss_list))
-        plt.savefig(f"loss_pic{model_idx}/loss{epochs}.png")
-        plt.show()
+        plt.savefig(f"figures/loss_pic/loss_pic{model_idx}/loss{epochs}.png")
         plt.cla()
-        
-        eval(model,testdataloader)
-        loss_list=[]
-        
+
+        eval(model, testdataloader)
+        loss_list = []
+
+
 idx_of_gen_traindata = 10
 inputs = []
 labels = []
-for idx_of_gen_traindata in [10,11,12,13,14,15,16,17,18,19]:
-    with h5py.File(f'data/dataset_{idx_of_gen_traindata}.h5','r') as gen_data:
+for idx_of_gen_traindata in [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]:
+    with h5py.File(f'data/dataset_{idx_of_gen_traindata}.h5', 'r') as gen_data:
         inputs.append(np.array(gen_data['inputs']))
-        labels.append(np.array(gen_data['labels'])[:,0:12])
-inputs = np.concatenate(inputs,axis=0)
-labels = np.concatenate(labels,axis=0)
+        labels.append(np.array(gen_data['labels'])[:, 0:12])
+inputs = np.concatenate(inputs, axis=0)
+labels = np.concatenate(labels, axis=0)
 print(f'total_len: {len(inputs)}')
 
 
-
-
-from torchvision.models import resnet34, resnet18,resnet101
 model_dict = {
-    18:resnet18,
-    34:resnet34,
-    101:resnet101
+    18: resnet18,
+    34: resnet34,
+    101: resnet101
 }
 model_idx = 18
 model = model_dict[model_idx](pretrained=False)
 model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-model.fc = nn.Linear(512,12)
+                        bias=False)
+model.fc = nn.Linear(512, 12)
 
 
-# model = pid_cnn_model()
+
 model.load_state_dict(torch.load(f'param/resnet{model_idx}.pth'))
 model.cuda()
 
-dataset = sevidataset(inputs[0:900],labels[0:900])
-testdataset = sevidataset(inputs[900:1000],labels[900:1000])
+dataset = sevidataset(inputs[0:900], labels[0:900])
+testdataset = sevidataset(inputs[900:1000], labels[900:1000])
 dataset.cuda()
 testdataset.cuda()
 dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 testdataloader = DataLoader(testdataset, batch_size=10, shuffle=False)
 
-train(model,dataloader,testdataloader,50)
-torch.save(model.state_dict(),f'param/resnet{model_idx}.pth')
-
+train(model, dataloader, testdataloader, 50)
+torch.save(model.state_dict(), f'param/resnet{model_idx}.pth')
